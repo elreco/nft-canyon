25c376ffb23c7c98b53140ce5ab9b4a0930a4a0a
Alexandre Le Corre <a.elreco@gmail.com>
2022-03-08 21:53:20 +0100

    Test lint staged

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,10 @@
-import Document, { Html, Main, Head, NextScript, type DocumentContext } from 'next/document'
+import Document, {
+  Html,
+  Main,
+  Head,
+  NextScript,
+  type DocumentContext
+} from 'next/document'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,14 @@
-import type { NextPage } from "next";
-import Header from '../components/header/Header';
-import Footer from '../components/footer/Footer';
+import type { NextPage } from 'next'
+import Header from '../components/header/Header'
+import Footer from '../components/footer/Footer'
 
 const Home: NextPage = () => {
   return (
-    <div className='home-1'>
-        <Header />
-        <Footer />
+    <div className="home-1">
+      <Header />
+      <Footer />
     </div>
-);
-};
+  )
+}
 
-export default Home;
+export default Home
