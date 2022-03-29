f2c653516a2de962fb48b0cec37a1c85ed40e21a
Alexandre Le Corre <a.elreco@gmail.com>
2022-03-29 18:47:57 +0200

    Nft Canyon

--- lib/isWalletConnected.ts
+++ lib/isWalletConnected.ts
@@ -1,7 +1,7 @@
 const isWalletConnected = async () => {
   const { ethereum } = window
   try {
-    if (!ethereum) return alert('Please install Metamask')
+    //if (!ethereum) return alert('Please install Metamask')
     const accounts = await ethereum.request({ method: 'eth_accounts' })
     if (accounts.length) {
       return accounts[0].toLowerCase()
